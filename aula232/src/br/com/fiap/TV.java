bd0a53cc1e020243f9ef7879654554c0f4603fd1
gabriel-riqueto <logonrmlocal@fiap.com.br>
2024-02-23 11:49:48 -0300

    aula java bglhs

--- aula232/src/br/com/fiap/TV.java
+++ aula232/src/br/com/fiap/TV.java
@@ -1,5 +1,20 @@
 package br.com.fiap;
 
 public class TV {
-
+	
+	public int vol;
+	public int canal;
+	
+	public void aumentarVol() {
+		vol ++;
+	}
+	
+	public void diminuirVol() {
+		vol--;
+	}
+	
+	public void trocarCanal(int e) {
+		canal = e;
+	}
+	
 }
